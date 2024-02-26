fcd24308e7a41b0dd6c2826a0254937951f30f35
MPasti <matheusspasti@gmail.com>
2024-02-26 16:09:35 -0300

    Criando componentes

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,21 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <>
-      <Post author="Matheus" content="teste" />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Matheus" content="teste" />
+          <Post author="Matheus" content="teste 2" />
+        </main>
+      </div>
     </>
   );
 }
