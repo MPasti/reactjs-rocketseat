40c665ff706809156c9227813a0fd4f5b20f6a24
MPasti <matheusspasti@gmail.com>
2024-02-28 11:57:13 -0300

    Criando novos componentes (sidebar)

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -4,6 +4,13 @@ export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <div className={styles.profile}>
+        <strong>Matheus Pasti</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
     </aside>
   );
 }
